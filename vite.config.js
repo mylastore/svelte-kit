f50b88d9987ad93d54d76c9a4a5977fdf5c8e438
mylastore <mrtonyq22@gmail.com>
2022-10-23 15:32:07 -0700

    run https on dev

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,30 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import fs from 'fs'
+import {variables} from "$lib/utils/variables"
 
-const options = {
-	key: fs.readFileSync('/Users/oscarquinteros/localhost-key.pem'),
-	cert: fs.readFileSync('/Users/oscarquinteros/localhost.pem'),
-};
+const isDev = variables.env === 'development'
+let options = {}
+let config
 
-/** @type {import('vite').UserConfig} */
-const config = {
-	server: {
-		https: {
-			key: options.key,
-			cert: options.cert
-		}
-	},
-	plugins: [sveltekit()]
-};
+if(isDev){
+	options = {
+		key: fs.readFileSync('/Users/oscarquinteros/localhost-key.pem'),
+		cert: fs.readFileSync('/Users/oscarquinteros/localhost.pem'),
+	}
+	config = {
+		server: {
+			https: {
+				key: options.key,
+				cert: options.cert
+			}
+		},
+		plugins: [sveltekit()]
+	}
+} else {
+	/** @type {import('vite').UserConfig} */
+	config = {
+		plugins: [sveltekit()]
+	}
+}
 
-export default config;
+export default config
