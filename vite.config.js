a09d092b6d93fa4e9807e7dd4c18dbefb3a3147d
mylastore <mrtonyq22@gmail.com>
2022-10-25 20:41:21 -0700

    run https on dev

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite'
-import fs from 'fs'
-
-// TODO isDev need to be change to false for production
-const isDev = false
-let options = {}
-
-if(isDev){
-	options = {
-		key: fs.readFileSync('/Users/oscarquinteros/localhost-key.pem'),
-		cert: fs.readFileSync('/Users/oscarquinteros/localhost.pem'),
-	}
-}
 
 const config = {
-	server: {
-		...(isDev ? {https: { key: options.key, cert: options.cert}} : null)
-	},
 	plugins: [sveltekit()]
 }
 
