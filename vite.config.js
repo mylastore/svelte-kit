8b30219e83559eb20496fa376feb9063f6f3337c
mylastore <mrtonyq22@gmail.com>
2022-11-17 20:52:40 -0800

    added SSL

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import dns from 'dns'
 import fs from "fs";
 
 dns.setDefaultResultOrder('verbatim')
-const isDev = true
+const isDev = false
 
 const key = isDev ? fs.readFileSync('/Users/oscarquinteros/localhost-key.pem') : fs.readFileSync('/home/admin/conf/web/ssl.sveltekit.mylastore.com.key')
 const pem = isDev ? fs.readFileSync('/Users/oscarquinteros/localhost.pem') : fs.readFileSync('/home/admin/conf/web/ssl.sveltekit.mylastore.com.pem')
