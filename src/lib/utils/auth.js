5ab8ea37911213b321551aebd647249bdbeb34f1
mylastore <mrtonyq22@gmail.com>
2022-11-17 21:35:07 -0800

    update remove cookie

--- src/lib/utils/auth.js
+++ src/lib/utils/auth.js
@@ -9,24 +9,15 @@ export const handleSession = async (res) => {
 
 export const logout = async () => {
   if (typeof window != 'undefined') {
-    await removeCookie('user',  {maxAge: 3.154e10})
+    Cookies.remove('user', { path: '' }) // removed!
     browser && localStorage.removeItem('username')
     window.location.replace('/')
   }
 }
 
-export const setCookie = async (key, value) => {
-  await Cookies.set(key, value)
-}
-
-export const removeCookie = async (key) => {
-  await Cookies.remove(key)
-
-}
-
 
 export const setUser = async (data) => {
   if (browser) {
-    await setCookie('user', data.user, {maxAge: 3.154e10})
+    await Cookies.set('user', data, { path: '' })
   }
 }
