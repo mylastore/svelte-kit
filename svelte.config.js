b01afced4848f0be678a8db6570c170db530e761
mylastore <mrtonyq22@gmail.com>
2022-11-19 22:03:52 -0800

    change HOST

--- svelte.config.js
+++ svelte.config.js
@@ -3,8 +3,9 @@ import adapter from '@sveltejs/adapter-node'
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
-		envPrefix: 'CUSTOMP'
+		adapter: adapter({
+			envPrefix: 'CUSTOMP'
+		}),
 	}
 };
 
