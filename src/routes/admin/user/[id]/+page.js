4c235ef9840af1ccd84c7436953ff63c8af5731b
mylastore <mrtonyq@me.com>
2023-04-11 00:04:06 -0700

    correction to user logout when token is not present.

--- src/routes/admin/user/[id]/+page.js
+++ src/routes/admin/user/[id]/+page.js
@@ -2,7 +2,7 @@ import { redirect } from '@sveltejs/kit';
 
 export async function load({parent}) {
   const session = await parent()
-  if (!session.token || session.user && session.user.role !== 'admin') {
+  if (session.user && session.user.role !== 'admin') {
     throw redirect(302, '/')
   }
 }
--- src/routes/register/+page.js
+++ src/routes/register/+page.js
@@ -2,7 +2,7 @@ import { redirect } from '@sveltejs/kit';
 
 export async function load({parent}) {
   const session = await parent()
-  if (session.token) {
+  if (session.user) {
     throw redirect(302, '/');
   }
 }
