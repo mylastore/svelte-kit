7bade27101aa67f82c204d41880614e5a6532fb9
mylastore <mrtonyq@me.com>
2023-04-16 12:11:23 -0700

    correction for dev env

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,32 @@
+import { defineConfig } from "vite"
 import {sveltekit} from '@sveltejs/kit/vite'
-import fs from 'fs'
+import fs from "fs"
 
-const isDev = true
+export default defineConfig(({ command }) => {
+  if (command === 'serve') {
+    const key = fs.readFileSync('secrets/certs/localhost.key')
+    const pem = fs.readFileSync('secrets/certs/localhost.crt')
 
-let key = isDev && fs.readFileSync('secrets/certs/localhost.key')
-let cert = isDev && fs.readFileSync('secrets/certs/localhost.crt')
-
-const conf = isDev ?
-  {
-    server: {
-      https: {
-        key: key,
-        cert: cert,
+    return {
+      server: {
+        https: {
+          key: key,
+          cert: pem,
+        },
+        host: 'localhost',
+        port: '3007',
+        strictPort: true
       },
-      host: 'localhost',
-      port: '3001',
-      strictPort: true
-    },
-    plugins: [sveltekit()]
+      plugins: [sveltekit()]
+    }
+  } else {
+    return {
+      server: {
+        host: 'localhost',
+        port: '3007',
+        strictPort: true
+      },
+      plugins: [sveltekit()]
+    }
   }
-  :
-  {
-    server: {
-      host: 'localhost',
-      port: '3007',
-      strictPort: true
-    },
-    plugins: [sveltekit()]
-  }
-
-  export default conf
+})
